31f4a40790a4ca39bc6c125299910ebbc7ed2cab
bassary <alexandre.hariri@orange.fr>
2025-11-14 12:10:27 +0100

    ajout favicon et compression de certain fichier svg

--- src/components/BannerMove.jsx
+++ src/components/BannerMove.jsx
@@ -2,12 +2,12 @@ function BannerMove() {
     return (
         <div className="conteneur__liste-image-defilement">
             <div className="liste-image-defilement">
-                <img className="image-defilement" src="assets/logo_lokowls.svg" alt="Site Lokowls"/>
+                <img className="image-defilement" src="assets/logo_lokowls.png" alt="Site Lokowls"/>
                 <img className="image-defilement" src="assets/logo_sophie_bluel.svg" alt="Site Sophie bluel architect"/>
                 <img className="image-defilement" src="assets/logo_argentbank.svg" alt="Site argentbank"/>
                 <img className="image-defilement" src="assets/logo_ohmyfood.svg" alt="Site ohmyfood"/>
         
-                <img className="image-defilement" src="assets/logo_lokowls.svg" alt="Site Lokowls"/>
+                <img className="image-defilement" src="assets/logo_lokowls.png" alt="Site Lokowls"/>
                 <img className="image-defilement" src="assets/logo_sophie_bluel.svg" alt="Site Sophie bluel architect"/>
                 <img className="image-defilement" src="assets/logo_argentbank.svg" alt="Site argentbank"/>
                 <img className="image-defilement" src="assets/logo_ohmyfood.svg" alt="Site ohmyfood"/>
