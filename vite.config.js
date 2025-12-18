c6d96ff09d6f718627ecc05c70c6abce30b1f13b
bassary <alexandre.hariri@orange.fr>
2025-12-18 15:23:24 +0100

    configuration de 404.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/404.html",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   base: "/",
 });
